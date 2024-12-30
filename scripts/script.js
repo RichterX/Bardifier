020307dbc08d7249e772ffcb8fcab1ea33bac4a3
Pedro Luque <3655082@gmail.com>
2024-12-30 10:09:18 +0100

    Some changes on background and buttons

--- scripts/script.js
+++ scripts/script.js
@@ -8,18 +8,15 @@ function toggleGrid(mode) {
     if (mode === 'vfx') {
         grid1.style.display = 'none';
         grid2.style.display = 'flex';
-        document.body.style.background = 'linear-gradient(to right, #d3d3d3, #a9a9a9)'; 
-        //Si quisiésemos cambiar el fondo por una imagen, podríamos hacerlo así:
-        // document.body.style.backgroundImage = 'url("vfx-background.jpg")'; // Imagen de fondo para VFX
-        // document.body.style.backgroundSize = 'cover'; // Para que la imagen cubra todo el fondo
+        document.body.style.background = 'linear-gradient(180deg,rgba(255, 188, 188, 0.76),rgb(49, 49, 118), transparent), url("./images/vfxbg.webp")'; 
+        document.body.style.backgroundSize = 'cover'; // Para que la imagen cubra todo el fondo
         currentGrid = 'vfx';
         controlButtons.style.display = 'none'; // Ocultar botones de control en VFX
     } else {
         grid1.style.display = 'flex';
         grid2.style.display = 'none';
-        document.body.style.background = 'linear-gradient(to right, #ffe4e1, #ffb6c1)';
-        // document.body.style.backgroundImage = 'url("vfx-background.jpg")'; // Imagen de fondo para VFX
-        // document.body.style.backgroundSize = 'cover'; // Para que la imagen cubra todo el fondo
+        document.body.style.background = 'linear-gradient(180deg,rgba(255, 188, 188, 0.76),rgb(49, 49, 118), transparent), url("./images/musicbg.jpg")'; 
+        document.body.style.backgroundSize = 'cover'; // Para que la imagen cubra todo el fondo
         currentGrid = 'music';
         controlButtons.style.display = 'block'; // Mostrar botones de control en Música
     }
